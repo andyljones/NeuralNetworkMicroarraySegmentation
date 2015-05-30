bd5076602ce73a5f998b4da1b3b0a20c66e845c6
Andy Jones <andyjones.ed@gmail.com>
2015-05-30 20:08:04 +0100

    Adds a module containing useful options for working interactively

---
+++ interactive_console_options.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 20:01:23 2015
+
+@author: andy
+"""
+
+import scipy as sp
+import logging
+import seaborn as sns
+
+logging.getLogger().setLevel(0)
+sp.set_printoptions(precision=3)
+sns.set_context(rc={'figure.figsize': (10, 10)})
