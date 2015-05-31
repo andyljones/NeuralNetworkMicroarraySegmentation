9539b49f79f1bbbdf58a97d27daf89472b4e5a23
Andy Jones <andyjones.ed@gmail.com>
2015-05-31 10:15:06 +0100

    Adds comments to experimental_tools

--- experimental_tools.py
+++ experimental_tools.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 30 12:15:52 2015
-
-@author: andy
+These are tools used by both the training and evaluation components of the experimental pipeline.
 """
 
 import tifffile
@@ -14,15 +12,18 @@ WINDOW_WIDTH = 61
 boundaries = [slice(650,-150), slice(1900,-500)]
 
 def get_benchmark_im(file_id):
+    """Gets the experimental image associated with ``file_id``"""
     filepath = os.path.join(IMAGE_FOLDER, file_id + '.tif')
     return sp.rollaxis(sp.log(tifffile.imread(filepath)), 0, 3)
 
 def get_bounded_im(file_id):
+    """Gets the experimental image associated with ``file_id``, then restricts it to the area with spots in"""
     im = get_benchmark_im(file_id)
     
-    return im[:, boundaries[0], boundaries[1]]
+    return im[boundaries[0], boundaries[1]]
 
 def get_bounded_ims():
+    """Gets all the experimental images, and restricts each to the area with spots in"""
     filenames = [f for f in os.listdir(IMAGE_FOLDER) if os.path.splitext(f)[1] == '.tif']
     file_ids = [filename.split('.tif')[0] for filename in filenames]
     return {file_id: get_bounded_im(file_id) for file_id in file_ids}
